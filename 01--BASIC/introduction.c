4bb74acc5d02c5456128458dc4177c95d8a19014
ANISH <anishthhakral09@gmail.com>
2026-07-11 17:13:02 +0530

    Add files via upload

---
+++ 01--BASIC/introduction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    //INTRODUCTION :
+    printf(
+        "Hello, My Name is Anish.\n"
+        "I am from Abohar, Punjab.\n"
+        "I have recently completed my 12th Standard and now,\n"
+        "I am pursuing Bachelor of Computer Applications(BCA) from D.A.V. Collage of Education, Abohar.\n"
+        "I am interested in Programming and Technology, and I enjoy learning new skills related to computers.\n"
+        "I also like the Video Editing and Making Content in my freetime.\n"
+        "My goal is to become skilled Software Developer and build a successful career in the IT Industry.\n"
+        "THANK YOU.\n");
+
+    return 0;
+}
